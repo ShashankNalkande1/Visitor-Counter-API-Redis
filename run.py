550b744d82749ffdf74da491d05edf0221a493c6
ShashankNalkande1 <shashank.nalkandecse2022@indoreinstitute.com>
2026-05-18 01:44:44 +0530

    update

--- run.py
+++ run.py
@@ -4,6 +4,6 @@ if __name__ == "__main__":
     uvicorn.run(
         "app.main:app",
         host="0.0.0.0",
-        port=8000,
+        port=1000,
         reload=True  # Convenient for development
     )
